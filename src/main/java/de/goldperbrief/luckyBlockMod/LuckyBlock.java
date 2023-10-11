5058822b25fd1eb8a4332de24c9a5b68ea0a1dc8
Minkampel <emil@grasskamp.de>
2023-10-11 02:57:26 +0200

    why aren´;t; you workign

--- src/main/java/de/goldperbrief/luckyBlockMod/LuckyBlock.java
+++ src/main/java/de/goldperbrief/luckyBlockMod/LuckyBlock.java
@@ -3,7 +3,11 @@ package de.goldperbrief.luckyBlockMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -13,13 +17,26 @@ import net.minecraft.world.World;
 public class LuckyBlock extends Block {
     public LuckyBlock(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(LUCK, 100));
     }
+
+    public static final IntProperty LUCK = IntProperty.of("luck",0,200);
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            player.sendMessage(Text.of("Hello, world!"), false);
+        if (hand.equals(Hand.MAIN_HAND) && ItemStack.EMPTY.itemMatches(player.getMainHandStack(), Item.byRawId(Item.getRawId(Items.EMERALD)))) {
+//            player.playSound(SoundEvents.BLOCK_NOTE_BLOCK_BIT, 1, 1);
+            world.setBlockState(pos, state.with(LUCK, +6));
+            return ActionResult.SUCCESS;
+        } else {
+            ActionResult.FAIL;
         }
+
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(LUCK);
     }
 }
